0d683d7de181b934a617312a07539f66cdc4234d
Rishaha <rishurawatx77@gmail.com>
2025-12-15 10:50:25 +0530

    we should use boundary instead of size in the loop.

--- Techniques/TwoPointers/fast_and_slow.cpp
+++ Techniques/TwoPointers/fast_and_slow.cpp
@@ -41,8 +41,7 @@ int main() {
    int element = 5;
    int boundary = removeElement(arr, size, element);
 
-
-   for(int i = 0; i < size; ++i) {
+   for(int i = 0; i < boundary; ++i) {
       cout << arr[i] << ' ';
    }
 
