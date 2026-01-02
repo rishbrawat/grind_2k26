025669967d852bde346e2948a751abf9ad3c3f0d
Rishaha <rishurawatx77@gmail.com>
2026-01-02 10:38:54 +0530

    umap revision

--- STL/Containers/u_map.cpp
+++ STL/Containers/u_map.cpp
@@ -1,75 +1,76 @@
 /*
+   an uordered_map is an associative container that stores the data in key:value pairs. where keys are unqiue, and elements are organized using a hashtable.
 
-   unordered_map is an associatvive container, that stores the data in a key - value pairs with unique keys, the data in the table is unsorted and it uses separate chaining method(lists).
-   avg : O(1) for insert, search, erase.
- 
-   also does rehashing if max_load_factor (a) > 1 (O(1): amortized)
-
-   the data is stored in a unsorted manner, for sorted data <map> is used which uses red-black tree.
-
-   When NOT to Use unordered_map
-
-    Need sorted order
-    Small constraints (use array)
-    Very tight memory
-    Key range is small & fixed
+   at high level
+   key->hashing_function(key)->index generated->store the value at that index.
 
 
+   core characteristics
+      no ordering of the elements.
+      each key is unique.
 
 */
 
-#include<unordered_map>
 #include<iostream>
-#include<string>
+#include<unordered_map>
 using namespace std;
 
 int main() {
-   // declare the map, keytype = string, valtype = int
-   unordered_map<string, int> mp;
-   // create a key "rishabh" if it does not exist and assign it a value of 25
-   mp["rishabh"] = 25;
+   // create an empty map
+   unordered_map<int, int> mp;
+   // insert an element
+   mp[0] = 20;
 
-   // insert method takes key value pairs inside the brackets {}, will insert only if the key does not exist, will ignore if key exists
-   mp.insert({"john", 20});
+   // using insert method: it wont overwrite the key if it exists already,
+   mp.insert({10, 20});
 
-   // find() searches for a key, returns an iterator if found, otherwise returns mp.end()
-   auto itr = mp.find("rishabh");
-   // correct usage
-   if(mp.find("rishabh") != mp.end()) {
-      cout << "key exists" << endl;
-   }
+   // direct access
+   int val = mp[0];
 
-   // count method for finding the existence of a key, returns 1 if found 0 if not
-   if(mp.count("rishabh")) {
-      cout << "key exists!" << endl;
-   }
-
-   // update the existing key
-   mp["john"] = 15;
-   mp["sonia"] = 12;
-   mp["ramesh"] = 17;
-   // iterate the map
-
-   for(auto& p: mp) {
-      cout << p.first << " " << p.second << '\n';
-   }
-   // erase method: erases a key and value, takes key as param
-
-   cout << endl;
-   mp.erase("ramesh");
-   for(auto& p: mp) {
-      cout << p.first << " " << p.second << '\n';
+   // check existence of a key
+   auto key = 0;
+   if(mp.find(key) != mp.end()) {
+      cout << "key found!" << '\n';
    }
 
 
+   // find frequency of item 
+   unordered_map<int, int> freq;
+   int arr[4] = {1,2,3,3};
+   for(auto x: arr) {
 
-   cout << "buckets available in the map: " << mp.bucket_count() << endl;
-   cout << "max buckets can be stored on map: " << mp.max_bucket_count() << endl;
-   cout << "load factor: " << mp.load_factor();
-   cout << "size of the map: " << mp.size() << endl;
-   // the lower is faster but wastes extra memory
-   mp.max_load_factor(5);
-   mp.clear(); // clear the map for preventing reallocation for buckets.
+      // okay, so its like for the first time, it just starts at 0 for every value, and on each iteration the x is updated. so its a freqeuncy counting.
+      freq[x]++;
+   }
+   // oww there are no indexes, so basically we are only storing values and frequencies.
+   for(auto& x: freq) {
+      cout << x.first << ' ' << x.second << '\n';
+   }
+   
+   cout << "value 3 appeared on map: " << freq[3] << " times" << endl;
 
-   return 0;
+
+   // find method
+   /*
+      find() method searches for key in the map, and returns an iterator.
+      if the key is not in the map, it returns end() which is iterator as well
+
+      end() means one past the last element, used only for comparisons, never dereference it
+
+      dont use freq[x] > 1 to check frequencies, it might insert the key
+
+
+      the method is useful for detecting duplicates in the map
+   */
+   auto it = freq.find(3);
+   if(it == freq.end()) {cout << "key not found in the map" << endl;}
+   else {cout << "key found in the map!"<<endl;}
+
+   if(it!=freq.end()) {
+      cout << it->second;
+   }
+
+
+   
+
 }
